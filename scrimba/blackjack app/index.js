ef2a60d4906ec1171f4c37f3e099b384d573db6c
Danna <46857905+dannachan@users.noreply.github.com>
2023-11-27 22:47:24 -0500

    more blackjack

--- scrimba/blackjack app/index.js
+++ scrimba/blackjack app/index.js
@@ -4,21 +4,35 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame(){
     if (sum <= 21){
         message = "Do you want to draw a new card?";
+        
     }else if (sum === 21){
         message = "You got a blackjack!";
         hasBlackJack = true;
+        
     }else {
         message = "You got a bust!";
         isAlive = false;
+        
     }
+    messageEl.textContent = message;
+    sumEl.textContent = `Sum: ${sum}`;
+    cardsEl.textContent = `Cards: ${firstCard} + ${secondCard}`;
 }
+
+function newCard(){
+    console.log("Drawing new card from the deck!");
+}
+
 
 //cash out money
 console.log(hasBlackJack);
 console.log(isAlive);
-console.log(message);
+
 
